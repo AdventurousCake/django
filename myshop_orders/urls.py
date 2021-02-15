ca6c04a119b969ae247df67dc68f7f575aaf5f53
i <tropitzyn.igor1@gmail.com>
2021-02-15 16:37:54 +0500

    style fix

--- myshop_orders/urls.py
+++ myshop_orders/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
-    # path('created/', views.order_created, name='order_created'),
     # path('^admin/order/(?P<order_id>\d+)/$', views.admin_order_detail, name='admin_order_detail'),
 ]
