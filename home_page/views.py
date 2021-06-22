c01624e2918b79cfca633d5d27ea6b2172a1fae8
i <tropitzyn.igor1@gmail.com>
2021-06-22 18:10:57 +0500

    upd bot status

--- home_page/views.py
+++ home_page/views.py
@@ -4,12 +4,19 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 import datetime
+import requests
 
 
 def index(request):
-    # return HttpResponse("hi")
-    data = {'date': str(datetime.datetime.now())}
+    req = requests.get("http://api1.testig.ml/ping")
+    if req.status_code != 200:
+        raise ConnectionError(f"Status code: {req.status_code}")
+
+    data = {'date': str(datetime.datetime.now()),
+            'bot': req.text,
+            }
     return render(request, 'home/index.html', data)
+    # return HttpResponse("hi")
 
 # def index(request):
 #     template = loader.get_template('main/index.html')
