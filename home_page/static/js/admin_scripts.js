60c8bf55ac475d9881582c110a063f07ee659670
i <tropitzyn.igor1@gmail.com>
2022-07-24 23:36:00 +0500

    upd script

--- home_page/static/js/admin_scripts.js
+++ home_page/static/js/admin_scripts.js
@@ -1,23 +1,24 @@
+"use strict";
 
-let el_id= "#content > h1"
+const el_id= "#content > h1"
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 24, 2022 15:37:25").getTime();
+const countDownDate = new Date("Jul 27, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  let distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   let time_str = "🔥 " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
