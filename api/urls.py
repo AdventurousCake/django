936449422d98b8f81431b1f79fc8f1fbf7b20f8f
i <tropitzyn.igor1@gmail.com>
2022-10-01 14:39:30 +0500

    upd api swagger

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from .views import hello, MessagesViewSet, UserList, MsgSearchViewSet, UserViewSet, MsgLoadView
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 
 router = DefaultRouter()
@@ -24,5 +27,18 @@ urlpatterns = [
 
     path('hello/', hello),
     path('api-token-auth/', views.obtain_auth_token),
-    path('users/<str:username>', UserList.as_view())
+    path('users/<str:username>', UserList.as_view()),
+
+    # swagger and schema
+    path('swagger', TemplateView.as_view(template_name='api/swagger-ui.html',
+                                         extra_context={'schema_url': 'openapi-schema'}),
+         name='swagger-ui'),
+    path('openapi', get_schema_view(
+            title="My Project",
+            description="API for all things …",
+            version="1.0.0",
+            permission_classes=(permissions.AllowAny,),
+            # public=True,
+        ), name='openapi-schema'),
+
 ]
